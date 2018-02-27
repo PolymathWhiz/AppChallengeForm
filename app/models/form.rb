b35a4d3baadc69218d1e38ce038a8afd53a97600
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2018-02-27 17:13:22 +0000

    added validation for name

--- app/models/form.rb
+++ app/models/form.rb
@@ -4,7 +4,12 @@ class Form < ApplicationRecord
 
   VALID_MOBILE_REGEX = /^[0-9]{10}$/
 
+  NAME_REGEX = /^[a-zA-Z\d\s]*$/
+
   validates_presence_of :first_name, :last_name, :category, :phone
+
+  validates :first_name, format: { with: NAME_REGEX, multiline: true }, allow_blank: true, length: { maximum: 30 }
+  validates :last_name, format: { with: NAME_REGEX, multiline: true }, allow_blank: true, length: { maximum: 30 }
 
   validates :phone, format: { with: VALID_MOBILE_REGEX, multiline: true }, allow_blank: true, length: { maximum: 10 }
 
